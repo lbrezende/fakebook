2bcc91e2f4b2c3e5e40ac9b743a278c4e7259231
Leandro Rezende <lbrezende@gmail.com>
2013-01-28 11:27:32 -0200

    heroku wake

--- config/initializers/heroku_wake_up.rb
+++ config/initializers/heroku_wake_up.rb
@@ -5,6 +5,6 @@ scheduler = Rufus::Scheduler.start_new
 scheduler.every '10m' do
   require "net/http"
   require "uri"
-  url = 'http://example.com'
+  url = 'http://fakebook-leandro.herokuapp.com'
   Net::HTTP.get_response(URI.parse(url))
 end
